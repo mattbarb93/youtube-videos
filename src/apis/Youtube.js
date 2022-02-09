1721de42d08306433e6a8e2fa9dc59747c5c6080
Matheus Barbosa <mattbarb93@gmail.com>
2022-02-09 15:17:08 -0500

    Updating API key + allowing user to make request

--- src/apis/Youtube.js
+++ src/apis/Youtube.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const KEY = "AIzaSyBGl0mbEk9WQeo3OCNgZjfOFRFmN3UUYLc";
+const KEY = "AIzaSyDurNcNh1VpdBbeJfAKm0SRFJFyENguvmQ";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
+    type: "video", //parameter to prevent "Warning: Each child in a list should have a unique "key" prop" warning message
     maxResults: 5,
     key: KEY,
   },
